fec038669bd9c3e71d9d3596d5aa741d076641b9
EveryPine <starcraft819@gmail.com>
2025-04-03 20:28:29 +0900

    chore: initial commit

---
+++ src/main/java/com/groommoa/aether_back_notification/AetherBackNotificationApplication.java
@@ -0,0 +1,13 @@
+package com.groommoa.aether_back_notification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AetherBackNotificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AetherBackNotificationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/groommoa/aether_back_notification/AetherBackNotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.groommoa.aether_back_notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AetherBackNotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
