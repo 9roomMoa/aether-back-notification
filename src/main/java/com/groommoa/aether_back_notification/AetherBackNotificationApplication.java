caa5a265cfd902c0b391895bbe02a91715b015c5
EveryPine <starcraft819@gmail.com>
2025-04-08 13:35:08 +0900

    build: mongodb audit 기능 사용 설정

--- src/main/java/com/groommoa/aether_back_notification/AetherBackNotificationApplication.java
+++ src/main/java/com/groommoa/aether_back_notification/AetherBackNotificationApplication.java
@@ -2,8 +2,10 @@ package com.groommoa.aether_back_notification;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 @SpringBootApplication
+@EnableMongoAuditing
 public class AetherBackNotificationApplication {
 
 	public static void main(String[] args) {
