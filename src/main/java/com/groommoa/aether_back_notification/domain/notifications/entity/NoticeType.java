76bd6d7dededa6235994b593f8fae8d6c99ebba2
EveryPine <starcraft819@gmail.com>
2025-04-08 22:00:49 +0900

    feat: NoticeType 수정
    - noticeType이 대소문자 구분 없이 파싱되도록 처리

--- src/main/java/com/groommoa/aether_back_notification/domain/notifications/entity/NoticeType.java
+++ src/main/java/com/groommoa/aether_back_notification/domain/notifications/entity/NoticeType.java
@@ -1,5 +1,6 @@
 package com.groommoa.aether_back_notification.domain.notifications.entity;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
@@ -14,4 +15,9 @@ public enum NoticeType {
     PROJECT_ADDED("project_added");
 
     private final String key;
+
+    @JsonCreator
+    public static NoticeType from(String value){
+        return value != null ? NoticeType.valueOf(value.toUpperCase()) : null;
+    }
 }
