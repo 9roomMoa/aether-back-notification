f4cb5a3e4d5929bf3d2d70d79369ad2946d79974
EveryPine <starcraft819@gmail.com>
2025-04-08 21:35:56 +0900

    chore: HttpStatus 수정
    - 다음 http 상태 코드 추가: 400, 500

--- src/main/java/com/groommoa/aether_back_notification/global/common/constants/HttpStatus.java
+++ src/main/java/com/groommoa/aether_back_notification/global/common/constants/HttpStatus.java
@@ -3,7 +3,9 @@ package com.groommoa.aether_back_notification.global.common.constants;
 public final class HttpStatus {
 
     public static final int OK = 200;
+    public static final int BAD_REQUEST = 400;
     public static final int UNAUTHORIZED = 401;
     public static final int MOVED_PERMANENTLY = 301;
+    public static final int INTERNAL_SERVER_ERROR = 500;
 }
 
